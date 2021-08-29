0fb3cbae8f8571d15c7933c4bd3b6fc028c17e6a
cyairahughes <59940992+cyairahughes@users.noreply.github.com>
2021-08-28 22:31:03 -0400

    Create Topping.java

---
+++ FreshmanYear(2019-2020)/DonutShop/Topping.java
@@ -0,0 +1,10 @@
+/****************************************************************************
+ * Topping
+ ****************************************************************************
+ * A program that keeps track of the customers and their donut purchases of a donut shop
+ *_____________________________________________________
+ * Cyaira Hughes
+ * April 8, 2020
+ * 255-901
+ ****************************************************************************/
+    public enum Topping{ NONE, SPRINKLES, NUTS, POWDERED_SUGAR, CEREAL, CANDY}
